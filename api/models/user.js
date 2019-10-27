9809e10c6f87bdb932c45a0c5f07475a9b3adf6c
201651027@iiitvadodara.ac.in <201651027@iiitvadodara.ac.in>
2019-10-27 16:42:35 +0530

    Version 4.0 Changes done

--- api/models/user.js
+++ api/models/user.js
@@ -5,7 +5,7 @@ const userSchema = mongoose.Schema({
   name : {type:String, required: true},
   email : {type:String, required: true},
   password : {type:String, required: true},
-  phone : {type:Number, require: true}
+  phone : {type:String, require: true}
 });
 
 module.exports = mongoose.model('User', userSchema);
